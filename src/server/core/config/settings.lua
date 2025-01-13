66230c328ca5719cad2cf466f46fd729387fd338
Samuel Spink <dr.greenjet@gmail.com>
2025-01-13 10:36:33 +0100

    settings: R15CharacterParts

--- src/server/core/config/settings.lua
+++ src/server/core/config/settings.lua
@@ -3,5 +3,6 @@ local SettingsMod = {}
 SettingsMod.RunInStudio = true
 
 SettingsMod.R6CharacterParts = {"Head", "Left Arm", "Left Leg", "Right Arm", "Right Leg", "Torso"};
+SettingsMod.R15CharacterParts = { "Head", "UpperTorso", "LowerTorso", "RightUpperArm", "RightLowerArm", "RightHand", "LeftUpperArm", "LeftLowerArm", "LeftHand", "RightUpperLeg", "RightLowerLeg", "RightFoot", "LeftUpperLeg", "LeftLowerLeg", "LeftFoot" }
 
 return SettingsMod
